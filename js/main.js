d4592a1ab6fcef0c658f319621bd38a3a45a4500
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-11-16 17:11:27 +0100

    Apportate modifiche importanti: - Adesso ogni email è rappresentato da un oggetto con il text (la mail stessa) e una proprietà 'clicked' impostata su false
    - Al click dell'icona per copiare la relativa email viene mostrato per due secondi un messaggio di conferma della copia

--- js/main.js
+++ js/main.js
@@ -4,20 +4,35 @@ createApp({
     data() {
         return {
             emailList: [],
+            copyMessage: 'Text copied successfully!',
+            newObjEmail: class {
+                constructor(text, clicked){
+                    this.text = text;
+                    this.clicked = clicked;
+                }
+            }
         }
     },
     methods:{
-        copyText: function(i) {
-            navigator.clipboard.writeText(this.emailList[i])
+        copyText: function(element) {
+            navigator.clipboard.writeText(element.text)
+            element.clicked = true;
+            setInterval(function(){
+                element.clicked = false;
+            }, 1500)
         }
     },
     created(){
         for(let i=1; i<=10; i++){
+
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
-                if(!this.emailList.includes(response.data.response)){
-                    this.emailList.push(response.data.response)
-                }
+                // if(!this.emailList.includes(response.data.response)){
+                //     this.emailList.push(response.data.response)
+                // }
+                const cratedObj = new this.newObjEmail(response.data.response, false);
+                this.emailList.push(cratedObj)
+
             });
         }
     }
