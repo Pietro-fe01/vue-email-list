5829bcd83f723524ce99f269652757b4cbf3c547
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-11-16 14:34:13 +0100

    Stampo in console l'indirizzo email generato dall'API di boolean

--- js/main.js
+++ js/main.js
@@ -3,10 +3,15 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            emailList: []
         }
     },
     methods:{
     },
     created(){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+            console.log(response.data.response)
+        });
     }
 }).mount('#app')
