d767b6e37dad113290020c7afe94ab799f20f878
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-11-16 17:31:43 +0100

    Eliminato righe codice inutilizzate js

--- js/main.js
+++ js/main.js
@@ -24,15 +24,10 @@ createApp({
     },
     created(){
         for(let i=1; i<=10; i++){
-
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
-                // if(!this.emailList.includes(response.data.response)){
-                //     this.emailList.push(response.data.response)
-                // }
                 const cratedObj = new this.newObjEmail(response.data.response, false);
                 this.emailList.push(cratedObj)
-
             });
         }
     }
