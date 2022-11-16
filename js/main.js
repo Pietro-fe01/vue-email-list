1a9bcb9a91b778185aa776312e5673583a1175f7
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-11-16 14:47:34 +0100

    Stampo in HTML le 10 email generate dall'API di boolean

--- js/main.js
+++ js/main.js
@@ -9,9 +9,11 @@ createApp({
     methods:{
     },
     created(){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((response) => {
-            console.log(response.data.response)
-        });
+        for(let i=1; i<=10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                this.emailList.push(response.data.response)
+            });
+        }
     }
 }).mount('#app')
