4674db574a94adb9b01373a0496bc4914cbebc96
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-11-16 15:41:57 +0100

    Creato una condizione che inserisce la mail generata dentro l'array solo se non è presente nella lista (la lista contiene solo mail univoche)

--- js/main.js
+++ js/main.js
@@ -12,7 +12,9 @@ createApp({
         for(let i=1; i<=10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
-                this.emailList.push(response.data.response)
+                if(!this.emailList.includes(response.data.response)){
+                    this.emailList.push(response.data.response)
+                }
             });
         }
     }
